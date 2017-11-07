390af66ae66988c5851acdabc3f4f43534f79594
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2017-11-07 09:48:35 -0500

    beginning sidebar

--- src/frontend/open_source/components/project/list.cljs
+++ src/frontend/open_source/components/project/list.cljs
@@ -23,7 +23,7 @@
        [:span.name (:project/name p)]
        [:span.tagline (:project/tagline p)]]
       (if (:project/beginner-friendly p)
-        [:div.beginner-friendly "beginner friendly"])
+        [:div.beginner-friendly [:i.fa.fa-check] " beginner friendly"])
       (if-let [t (:record/tags p)]
         [:div.tags
          (for [tag (u/split-tags t)]
@@ -43,20 +43,50 @@
 
 (defn project-list
   []
-  [:table.projects
-   [:thead
-    [:tr
-     [:th "project"]
-     [:th] [:th]
-     [:th [:i.fa.fa-star]]
-     [:th [:i.fa.fa-clock-o]]]]
-   [:tbody
-    (->> @(rf/subscribe [:projects])
-         vals
-         (sort-by :slug)
-         (map (fn [p]
-                ^{:key (str "os-project-" (:slug p))}
-                [project p])))]])
+  [:div.main.listings.public
+   [:table.projects
+    [:thead
+     [:tr
+      [:th "project"]
+      [:th] [:th]
+      [:th [:i.fa.fa-star]]
+      [:th [:i.fa.fa-clock-o]]]]
+    [:tbody
+     (->> @(rf/subscribe [:projects])
+          vals
+          (sort-by :slug)
+          (map (fn [p]
+                 ^{:key (str "os-project-" (:slug p))}
+                 [project p])))]]])
+
+(defn sidebar
+  []
+  (let [{:keys [input]} (stfc/form [:projects :search])
+        selected-tags []
+        tags []]
+    [:div.secondary.listings
+     [:div.section.search
+      [input :search :query
+       :no-label true
+       :placeholder "Search: `music`, `database` ..."]]
+     [:div.section.beginner-toggle
+      [input :checkbox :project/beginner-friendly :label "Beginner friendly?"]]
+     [:div.section.tags
+      [:div
+       (for [tag tags]
+         ^{:key (gensym)} [filter-tag selected-tags tag])]]
+     [:div.section
+      [:div.details
+       [:h3 "Learn Clojure"]
+       [:div.book-cover [:img {:src "/images/book-cover.png"}]]
+       [:p [:a {:href "http://braveclojure.com"} [:em "Clojure for the Brave and True"]]
+        " is a fun, in-depth introduction to Clojure. Read
+                   it "
+        [:a {:href "http://braveclojure.com"} "free online"]
+        " or "
+        [:a {:href "http://amzn.to/1mz8qNR"} "buy the print
+                   or ebook on Amazon"]
+        "!"]]]]))
 
 (defn component
   []
@@ -66,56 +96,5 @@
      [:p "These projects are under active development and welcome new contributors."]]
     [:div.secondary
      [:a {:href "/projects/new"} "Post Your Project"]]]
-   [:div.main.listings.public
-    [project-list]]])
-
-(defn view
-  []
-  (let [listings        @(rf/subscribe [:filtered-projects])
-        tags            @(rf/subscribe [:project-tags])
-        selected-tags   @(rf/subscribe [:key :forms :projects :search :data :tags])
-        {:keys [input]} (stfc/form [:projects :search])]
-    [:div.list
-     [:div.intro
-      [:div.main
-       [:p "Looking to improve your skills and work with real code? These projects are under active development and welcome new contributors."]]
-      [:div.secondary
-       [:button.submit.target "Post Your Project"]]]
-     [:div.main.listings.public
-      [ui/ctg {:transitionName "filter-survivor" :class "listing-list"}
-       (for [l listings]
-         ^{:key (str "os-project-" (:slug l))}
-         [:div.listing-container
-          [:a.listing.clearfix {:href (:slug l)}
-           [:div.core
-            [:div.title [ui/attr l :project/name]]
-            [ui/attr l :project/tagline]
-            (if (:project/beginner-friendly l)
-              [:div.beginner-friendly "beginner friendly"])
-            (if-let [t (:record/tags l)]
-              [:div.tags
-               (for [tag (u/split-tags t)]
-                 ^{:key (gensym)} [filter-tag selected-tags tag])])]]])]]
-     [:div.secondary.listings
-      [:div.section.search
-       [input :search :query
-        :no-label true
-        :placeholder "Search: `music`, `database` ..."]]
-      [:div.section.beginner-toggle
-       [input :checkbox :project/beginner-friendly :label "Beginner friendly?"]]
-      [:div.section.tags
-       [:div
-        (for [tag tags]
-          ^{:key (gensym)} [filter-tag selected-tags tag])]]
-      [:div.section
-       [:div.details
-        [:h3 "Learn Clojure"]
-        [:div.book-cover [:img {:src "/images/book-cover.png"}]]
-        [:p [:a {:href "http://braveclojure.com"} [:em "Clojure for the Brave and True"]]
-         " is a fun, in-depth introduction to Clojure. Read
-                   it "
-         [:a {:href "http://braveclojure.com"} "free online"]
-         " or "
-         [:a {:href "http://amzn.to/1mz8qNR"} "buy the print
-                   or ebook on Amazon"]
-         "!"]]]]]))
+   [project-list]
+   [sidebar]])
